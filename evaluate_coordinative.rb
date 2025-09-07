54ecdc9a8ceb9e1c2db3a068ba79ad3715322130
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-09-07 16:18:47 +0000

    Update evaluate_coordinative.rb

--- evaluate_coordinative.rb
+++ evaluate_coordinative.rb
@@ -13,3 +13,21 @@ def coordinative_selective
                                                            
   SaadSelective::CoordinatedSelection.decrement_confidence
 end
+
+## Reassess Player
+def reasses_player; SaadSelective::CompetitiveSelection.reasses_player; end
+
+## Reassess Gribatomaton
+def reasses_gribatomaton; SaadSelective::CompetitiveSelection.reasses_gribatomaton; end
+
+## Reassess Enemy
+def reasses_enemy; SaadSelective::CompetitiveSelection.reasses_enemy; end
+
+## Reconsider Player
+def reasses_player; SaadSelective::CompetitiveSelection.reconsider_player; end
+
+## Reconsider Gribatomaton
+def reasses_gribatomaton; SaadSelective::CompetitiveSelection.reconsider_gribatomaton; end
+
+## Reconsider Enemy
+def reasses_enemy; SaadSelective::CompetitiveSelection.reconsider_enemy; end
