6838c3126d666b876d9d21c28b22d9ef312491c7
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-09-16 17:50:36 +0000

    Update evaluate_coordinative.rb

--- evaluate_coordinative.rb
+++ evaluate_coordinative.rb
@@ -1,51 +1,56 @@
 require_relative "SelectiveSaad/SelectiveSaad.rb"
 
-def coordinative_selective
-  SaadSelective::CoordinativeSelection.get_player_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
+def get_statistics
+  SaadSelective::CoordinatedSelection.get_player_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
                                                             :cult_actors,      "Those whom have previously been part of cults, tend to be more likely to take on dangerous economic espionage relationships based on cheating mmos.",
                                                             :cults_to_mmos,    "Starving prisoners had previously been members of advanced persistent threats, before moving onto cheating in MMOs.")
 
   #puts "The current player probability is: #{@current_player_probability}"
   
-  SaadSelective::CoordinativeSelection.get_gribatomaton_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
+  SaadSelective::CoordinatedSelection.get_gribatomaton_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
                                                                   :cult_actors,      "Those whom have previously been part of cults, tend to be more likely to take on dangerous economic espionage relationships based on cheating mmos.",
                                                                   :cults_to_mmos,    "Starving prisoners had previously been members of advanced persistent threats, before moving onto cheating in MMOs.")
 
   #puts "The current gribatomaton probability is: #{@current_gribatomaton_probability}"
   
-  SaadSelective::CoordinativeSelection.get_enemy_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
+  SaadSelective::CoordinatedSelection.get_enemy_statistics(:starving_threats, "Starving prisoners are more likely to improve their life circumstances by rising in ranks to become advanced persistent threats.",
                                                             :cult_actors,      "Those whom have previously been part of cults, tend to be more likely to take on dangerous economic espionage relationships based on cheating mmos.",
                                                             :cults_to_mmos,    "Starving prisoners had previously been members of advanced persistent threats, before moving onto cheating in MMOs.")
 end
 
-def increment_confidence
-  SaadSelective::CoordinativeSelection.increment_confidence
+def decrement_confidence
+  SaadSelective::CoordinatedSelection.decrement_confidence
 end
 
 def current_information
-  SaadSelective::CoordinativeSelection.current_information
+  SaadSelective::CoordinatedSelection.current_information
 end
 
 ## Reassess Player
-def reasses_player; SaadSelective::CoordinativeSelection.reasses_player; end
+def reasses_player; SaadSelective::CoordinatedSelection.reasses_player; end
 
 ## Reassess Gribatomaton
-def reasses_gribatomaton; SaadSelective::CoordinativeSelection.reasses_gribatomaton; end
+def reasses_gribatomaton; SaadSelective::CoordinatedSelection.reasses_gribatomaton; end
 
 ## Reassess Enemy
-def reasses_enemy; SaadSelective::CoordinativeSelection.reasses_enemy; end
+def reasses_enemy; SaadSelective::CoordinatedSelection.reasses_enemy; end
 
 ## Reconsider Player
-def reconsider_player; SaadSelective::CoordinativeSelection.reconsider_player; end
+def reconsider_player; SaadSelective::CoordinatedSelection.reconsider_player; end
 
 ## Reconsider Gribatomaton
-def reconsider_gribatomaton; SaadSelective::CoordinativeSelection.reconsider_gribatomaton; end
+def reconsider_gribatomaton; SaadSelective::CoordinatedSelection.reconsider_gribatomaton; end
 
 ## Reconsider Enemy
-def reconsider_enemy; SaadSelective::CoordinativeSelection.reconsider_enemy; end
+def reconsider_enemy; SaadSelective::CoordinatedSelection.reconsider_enemy; end
 
-coordinative_selective
-decrement_confidence
-decrement_confidence
+get_statistics
 
+3.times do
+  reasses_player       #.class
+  reasses_gribatomaton #.class
+  reasses_enemy        #.class
+end
+
+decrement_confidence
 puts current_information
